3d0cba268a275b2122669e466972626c1ba14da8
Viktoria <vorobeva.vik.99@mail.ru>
2023-03-22 10:02:29 +0300

    добавляет правки по стилям в JS

--- source/js/modules/breakpoints.js
+++ source/js/modules/breakpoints.js
@@ -1,6 +1,6 @@
 const breakpoint = window.matchMedia('(max-width:767px)');
 const mainScreenButton = document.querySelector('.main-screen__link');
-const catalogText = document.querySelector('.catalog__text');
+const catalogText = document.querySelector('.catalog h2');
 
 const breakpointChecker = () => {
   if (mainScreenButton && breakpoint.matches) {
