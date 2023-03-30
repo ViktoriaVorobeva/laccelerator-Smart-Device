e1a98d6eb65a55b49e473f45e4b2d65d38060298
Viktoria <vorobeva.vik.99@mail.ru>
2023-03-30 16:37:37 +0300

    добавляет правки по форме

--- source/js/modules/form-validate/callback.js
+++ source/js/modules/form-validate/callback.js
@@ -1,6 +1,44 @@
+const submitButtonElement = document.querySelector('button[type="submit"]');
+
+const blockSubmitButton = () => {
+  submitButtonElement.disabled = true;
+  submitButtonElement.textContent = 'Отправляю...';
+};
+
+const unblockSubmitButton = () => {
+  submitButtonElement.disabled = false;
+  submitButtonElement.textContent = 'Задать вопрос';
+};
+
+const sendData = (onSuccess, body) => {
+  fetch(
+    'https://echo.htmlacademy.ru',
+    {
+      method: 'POST',
+      body,
+    },
+)
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        alert('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
+    .catch(() => {
+      alert('Не удалось отправить форму. Попробуйте ещё раз');
+    });
+};
+
 
 const baseSuccessCallback = (event) => {
   event.preventDefault();
+  blockSubmitButton();
+  sendData(() => {
+    alert('Форма отправлена');
+    unblockSubmitButton();
+  },
+  new FormData(event.target));
   // В данном колбеке бэкендер, либо разработчик при необходимости будет писать запрос на отправку формы на сервер и обрабатывать возможные ошибки или успешную отправку формы на сервер
 };
 
